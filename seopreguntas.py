59bb82ec4d213270c5ac0d0728cabf3ba3550d78
Gabo1123 <104657379+Gabo1123@users.noreply.github.com>
2023-04-06 20:43:51 -0500

    Update seopreguntas.py

--- seopreguntas.py
+++ seopreguntas.py
@@ -1,34 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
 import streamlit as st
-from googleapiclient.discovery import build
-import re
 
-# Reemplaza YOUR_API_KEY con tu clave de API
-API_KEY = "YOUR_API_KEY"
-SEARCH_ENGINE_ID = "012345678901234567890:abcdefghijklm"
+st.title("Extractor de Preguntas de Bing")
 
-# Función para extraer preguntas de los resultados de búsqueda
-def extract_questions(query, num_results=20):
-    questions = []
-    service = build("customsearch", "v1", developerKey=API_KEY)
-    response = service.cse().list(q=query, cx=SEARCH_ENGINE_ID, num=num_results).execute()
-    
-    for item in response.get("items", []):
-        title = item["title"]
-        if re.search("(?i)(cómo|cuándo|dónde|por qué|para qué|cuál|quiénes|qué)", title):
-            questions.append(title)
-    return questions
-
-# Aplicación Streamlit
-st.title("Extractor de Preguntas de Google SERPs")
+# Entrada de la palabra clave
 keyword = st.text_input("Ingresa la palabra clave:")
-num_results = st.number_input("Número de resultados a considerar (máx. 100):", min_value=20, max_value=100, value=20)
+
+# Palabras clave de las preguntas
+question_words = ["cómo", "cuándo", "dónde", "por qué", "para qué"]
+
+def fetch_search_results(keyword, count=20):
+    url = f"https://www.bing.com/search?q={keyword}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    search_results = soup.find_all("li", class_="b_algo")
+
+    questions = []
+    for result in search_results:
+        title = result.find("a").text
+        for word in question_words:
+            if word in title.lower():
+                questions.append(title)
+                break
+    return questions[:count]
 
 if keyword:
-    query = f"{keyword} intitle:(cómo OR cuándo OR dónde OR por qué OR para qué OR cuál OR quiénes OR qué)"
-    questions = extract_questions(query, num_results)
+    questions = fetch_search_results(keyword)
     if questions:
-        st.header("Preguntas encontradas:")
+        st.subheader("Preguntas encontradas:")
         for question in questions:
             st.write(question)
     else:
-        st.warning("No se encontraron preguntas relacionadas con la palabra clave.")
+        st.write("No se encontraron preguntas relacionadas con la palabra clave.")
